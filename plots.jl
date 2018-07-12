78f1858b605f06a1822fab40c283f11f7db849bf
prithvi <prith@umich.edu>
2018-07-12 15:16:33 -0400

    testing 1; add data folder

--- plots.jl
+++ plots.jl
@@ -15,3 +15,21 @@ fricPlot = plot(layer(x = (cca-ccb).*1e3 + 10, y = FltX/1e3,
                 Guide.ylabel("Depth (m)"),
                 Guide.title("Rate and state friction/Stress"),
                 Coord.Cartesian(ymin=-24))
+
+# Plot shear stress at location as a function of time
+loc1 = 3e3  # 3 km depth
+FltID = find(abs.(FltX) .< loc1)[1]
+
+shearPlot = plot(x = time_./yr2sec, y = Stress[FltID,:], Geom.line,
+                Guide.xlabel("Time (yr)"),
+                Guide.ylabel("Shear stress at location 1"),
+                Guide.title("Shear Stress as a function of time"),
+                Coord.Cartesian(ymin=10, ymax = 60, xmax = 1000))
+
+
+# Plot slip velocity at location as a function of time (same location)
+slipVelPlot = plot(x = time_./yr2sec, y = SlipVel[FltID,:], Geom.line,
+                Guide.xlabel("Time (yr)"),
+                Guide.ylabel("Shear stress at location 1"),
+                Guide.title("Shear Stress as a function of time"),
+                Coord.Cartesian(ymin=0, ymax = 5, xmax = 1000))
