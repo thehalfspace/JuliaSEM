0d60aad44d5725cc19f9d84bd767fd3d52f67f35
prithvi <prith@umich.edu>
2019-01-18 10:47:32 -0500

    fourier stress stuff

--- scripts_old/scripts/stress_fft.jl
+++ scripts_old/scripts/stress_fft.jl
@@ -7,16 +7,13 @@
 using FFTW
 using LinearAlgebra
 function stress_fft(Stress, FltX, start_indx, end_indx, evno, time_)
-    
-
-    x1, x2 = findmax(FltX .>= -8e3)
-
+    #  x1, x2 = findmax(FltX .>= -8e3)
     i1 = Int(start_indx[evno])
     i2 = Int(end_indx[evno])
-     str = Stress[x2,:]
-    #  str = Stress[:,Int.(start_indx)]
+     #  str = Stress[:,]
+    str = Stress[:,Int.(start_indx)]
 
-    stress_amp = abs.(fft(str,1))
+    stress_amp = abs.(fft(str[1:end-1,:],1))
     
     #  p1 = plot(x=stress_amp, y=FltX./1e3, Geom.line,
          #  Guide.title("Shear Stress for the Start of Each Event"),
@@ -26,12 +23,34 @@ function stress_fft(Stress, FltX, start_indx, end_indx, evno, time_)
     fig = PyPlot.figure(figsize=(12,8), dpi = 120)
     ax = fig[:add_subplot](111)
 
-    ax[:plot](time_, stress_amp, "ko", lw = 1)
-    ax[:set_xlabel]("Time (yr)")
-    ax[:set_ylabel]("Shear Stress Fourier Amplitude")
-    #  ax[:set_xscale]("log")
+    ax[:plot](stress_amp, -1e3 ./FltX[1:end-1], "ko", lw = 1)
+    ax[:set_xlabel]("Shear Stress in fourier domain")
+    ax[:set_ylabel]("Depth frequency (km^{-1})")
+    ax[:set_xscale]("log")
     ax[:set_yscale]("log")
-    ax[:set_title]("Shear stress fourier amplitude at 8km depth")
-    ax[:set_ylim]([-24, 0])
+    ax[:set_title]("Shear stress fourier amplitude at the start of each event")
+    #  ax[:set_ylim]([-24, 0])
+    ax[:invert_yaxis]()
     show()
+    figname = string(path, "fourier_shear_stress.pdf")
+    fig[:savefig](figname, dpi = 300)
+end
+
+function stress_fft2(stressdrops, FltX)
+    stress_amp = abs.(fft(stressdrops[361:end-1,:],1))
+
+    fig = PyPlot.figure(figsize=(12,8), dpi = 120)
+    ax = fig[:add_subplot](111)
+
+    ax[:plot](stress_amp, FltX[361:end-1]./1e3, "ko--", lw = 1)
+    ax[:set_xlabel]("Stress drops in fourier domain")
+    ax[:set_ylabel]("Depth frequency (km^-1)")
+    ax[:set_xscale]("log")
+    #  ax[:set_yscale]("log")
+    ax[:set_title]("Stress drop fourier amplitude for each event")
+    #  ax[:set_ylim]([-24, 0])
+    #  ax[:invert_yaxis]()
+    show()
+    figname = string(path, "fourier_stress_drop.pdf")
+    fig[:savefig](figname, dpi = 300)
 end
